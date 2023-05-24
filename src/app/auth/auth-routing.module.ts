910f37e4586e6d61833f110b14b87a716c1307c7
Christopher Christensen <cch@goollie.com>
2023-05-24 17:40:23 +0200

    auth.guard

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthComponent,
+    canActivate: [() => inject(AuthGuard).canActivate]
   },
+  
 ];
 
 @NgModule({
