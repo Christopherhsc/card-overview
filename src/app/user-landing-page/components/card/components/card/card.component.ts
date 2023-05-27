ea367d78f16926c49f8593e85d6cf6db9dd023c2
Christopherhsc <Christopherhsc2520@gmail.com>
2023-05-27 16:07:56 +0200

    card components with ME

--- src/app/user-landing-page/components/card/components/card/card.component.ts
+++ src/app/user-landing-page/components/card/components/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CardService } from '../../card.service';
 import { Card } from '../../card.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -9,9 +11,14 @@ import { Card } from '../../card.model';
 })
 export class CardComponent implements OnInit {
   loadedCards!: Card[];
-  constructor(private cardService: CardService) {}
+  constructor(private cardService: CardService, private router: Router) {}
 
   ngOnInit() {
     this.loadedCards = this.cardService.cards;
+  }
+
+  editCard(cardId: string, slidingItem: IonItemSliding){
+    slidingItem.close()
+    this.router.navigate(['/', cardId])
   }
 }
